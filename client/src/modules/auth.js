503d4821419a69d54e3eec4c1166de5b287fee2e
jungsikjeong <wndtlr1024@gmail.com>
2020-06-25 12:53:20 +0900

    Add: auth 리덕스 모듈

--- client/src/modules/auth.js
+++ client/src/modules/auth.js
@@ -69,6 +69,33 @@ const auth = handleActions(
       ...state,
       [form]: { ...state[form], [key]: value },
     }),
+    [INITIALIZE_FORM]: (state, { payload: { form } }) => ({
+      ...state,
+      [form]: initialState[form], // state초기화
+      authError: null, // 폼 전환 시 회원 인증 에러 초기화
+    }),
+    // 회원가입 성공
+    [REGISTER_SUCCESS]: (state, { payload: { auth } }) => ({
+      ...state,
+      auth,
+      authError: null,
+    }),
+    // 회원가입 실패
+    [REGISTER_FAILURE]: (state, { payload: { error } }) => ({
+      ...state,
+      authError: error,
+    }),
+    // 로그인 성공
+    [LOGIN_SUCCESS]: (state, { payload: auth }) => ({
+      ...state,
+      authError: null,
+      auth,
+    }),
+    // 로그인 실패
+    [LOGIN_FAILURE]: (state, { payload: error }) => ({
+      ...state,
+      authError: error,
+    }),
   },
   initialState,
 );
