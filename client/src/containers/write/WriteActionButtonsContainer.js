bd8b3bc2b256b45044f7b64075feea12f2b1d07b
jungsikjeong <wndtlr1024@gmail.com>
2020-08-06 04:01:38 +0900

    Add : alert

--- client/src/containers/write/WriteActionButtonsContainer.js
+++ client/src/containers/write/WriteActionButtonsContainer.js
@@ -6,13 +6,19 @@ import { writePost } from '../../modules/write';
 const WriteActionButtonsContainer = ({ onCanvasSave }) => {
   const dispatch = useDispatch();
 
-  const { title, body, canvasData } = useSelector(({ write }) => ({
+  const { title, body, canvasData, isData } = useSelector(({ write }) => ({
     title: write.title,
     body: write.body,
     canvasData: write.canvasData.canvasData,
+    isData: write.isData,
   }));
 
   const onPublish = () => {
+    if (!isData) {
+      alert('그림저장 버튼을 먼저 눌러주세요!');
+      return;
+    }
+
     dispatch(
       writePost({
         title,
